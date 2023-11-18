afccd9bcf06f9149683525f91caf1515b06cfddb
Olesya K <olesjakraineva@gmail.com>
2023-11-18 13:45:47 +0300

    Add seminar 1

---
+++ Sem_01/Program.cs
@@ -0,0 +1,111 @@
+﻿// Задача 1
+// //типДанный имяПеременной = значение;
+// int number = -5; //number = 5
+// int result = number * number; // result - это number, возведённый в квадрат
+// //Печатаем результат
+// Console.Write("Результат: " + result);
+
+
+// Задача 2
+// int a = Convert.ToInt32("13");
+// Console.WriteLine(a + a);
+// int a = Convert.ToInt32("12");
+// Console.WriteLine(a + a); // 12 + 12
+
+
+//Задача 3
+// int temp = 2;
+// if (temp >= 10  && temp < 15)
+// {
+//     Console.WriteLine("Куртку");
+// }
+// else if (temp > 15 && temp <= 20)
+// {
+//     Console.WriteLine("Лёгкую Куртку");
+// }
+// else if (temp > 20)
+// {
+//     Console.WriteLine("Кепку");
+// }
+// else
+// {
+//     Console.WriteLine("Температура меньше 10 градусов");
+// }
+
+//Задача 4
+// Console.Write("Введите номер дня недели: ");
+// int numberOfDays = Convert.ToInt32(Console.ReadLine());
+// //int numberOfDays = Convert.ToInt32(Console.ReadLine());
+// // "==" - проверка на равенство 
+// if (numberOfDays == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (numberOfDays == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (numberOfDays == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (numberOfDays == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (numberOfDays == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (numberOfDays == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (numberOfDays == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Неправильно введено значение недели");
+// }
+
+
+//Задача 5
+// string test = "Hi ";
+
+// if (test == "Hi")
+// {
+//     System.Console.WriteLine("+");
+// }
+// else
+// {
+//     System.Console.WriteLine("-");
+// }
+
+//Задача 6
+// Console.Write("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// if (N < 0)
+// {
+//     N *=-1; //если вводят отрицательное число
+// }
+// int NegativN = N * -1;
+// while (NegativN <= N) // пока NegativN не равно N (-2, -1, 0, 1, 2)
+// {
+//     Console.Write(NegativN + " ");
+//     //NegativN++; // увелич. на 1 ед.
+//     NegativN += 1; // NegativN = NegativN + 1
+// }
+
+
+//5 % 2 = 1 (не чётное)
+//4 % 2 = 0 (чётное)
+
+// if (num % 2 == 0) 
+// {
+    
+// }
+
+// int num = 2;
+// int next = num + 2;
